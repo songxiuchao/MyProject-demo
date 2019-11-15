974a5daaab9fb8f5facebdaebde64878d1385e58
songxiuchao <15666131983>
2019-11-15 12:02:14 +0800

    新增JDK8新特性

--- src/main/java/com/example/demo/JDK8/lambda/Main.java
+++ src/main/java/com/example/demo/JDK8/lambda/Main.java
@@ -37,6 +37,16 @@ public class Main {
 	interface MathOperation {
 		int operation(int a, int b);
 	}
+
+	/**
+	 * 接口有且仅有一个抽象方法
+	 * 允许定义静态方法
+	 * 允许定义默认方法
+	 * 允许java.lang.Object中的public方法
+	 * 该注解不是必须的，如果一个接口符合"函数式接口"定义，那么加不加该注解都没有影响。
+	 * 加上该注解能够更好地让编译器进行检查。如果编写的不是函数式接口，
+	 * 但是加上了@FunctionInterface，那么编译器会报错
+	 */
 	@FunctionalInterface
 	interface GreetingService {
 		void sayMessage(String message);
