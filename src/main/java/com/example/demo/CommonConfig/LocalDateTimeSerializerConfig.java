05bed3506a2c55e17a79a0fa6c624c9ebac396aa
songxiuchao <1528856973@qq.com>
2020-04-13 15:55:38 +0800

    全局事件配置

---
+++ src/main/java/com/example/demo/CommonConfig/LocalDateTimeSerializerConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.CommonConfig;
+
+/**
+ * @Author: xiaofu
+ * @Description:
+ */
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @Description: 全局时间配置
+ */
+@Configuration
+public class LocalDateTimeSerializerConfig {
+
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeDeserializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+    }
+}
+
